118c43c546cfd78f4d274aad4d8bae33874e45a6
Marvin <marvin.tejada@orangeandbronze.com>
2014-05-29 15:36:46 +0800

    another sysout

--- src/com/example/Runner.java
+++ src/com/example/Runner.java
@@ -5,5 +5,6 @@ public class Runner {
 	public static void main(String args[]){
 		System.out.println("Hello world");
 		System.out.println("another sysout");
+		System.out.println("yey new branch");
 	}
 }

dd934f83842975fa551af1ec8c4b7038a98eeac6
Marvin <marvin.tejada@orangeandbronze.com>
2014-05-29 16:03:22 +0800

    added third sysout

--- src/com/example/Runner.java
+++ src/com/example/Runner.java
@@ -5,6 +5,6 @@ public class Runner {
 	public static void main(String args[]){
 		System.out.println("Hello world");
 		System.out.println("another sysout");
-		System.out.println("yey new branch");
+		System.out.println("third println");
 	}
 }
